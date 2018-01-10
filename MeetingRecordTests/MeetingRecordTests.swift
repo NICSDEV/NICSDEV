dbc8666bffdd74cfb8b549541f41a32224a8bde0
NICSDEV <nagamori@nics.co.jp>
2018-01-10 10:29:13 +0900

    Initial Commit

---
+++ MeetingRecordTests/MeetingRecordTests.swift
@@ -0,0 +1,36 @@
+//
+//  MeetingRecordTests.swift
+//  MeetingRecordTests
+//
+//  Created by nics1094 on 2018/01/10.
+//  Copyright © 2018年 nics1094. All rights reserved.
+//
+
+import XCTest
+@testable import MeetingRecord
+
+class MeetingRecordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
